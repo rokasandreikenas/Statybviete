3348a63acdf44c4c902927ff0076fdb59a715382
rokasandreikenas <shyptt@gmail.com>
2020-10-06 15:49:07 +0300

    add table design

--- src/components/EstimateCalculator/EstimateTable/EstimateTable.js
+++ src/components/EstimateCalculator/EstimateTable/EstimateTable.js
@@ -5,26 +5,28 @@ import "./EstimateTable.scss";
 
 const EstimateTable = ({ list, inputs, handleChange }) => {
   return (
-    <table className="estimate-table">
-      <TableHeader />
-      <tbody>
-        {list.map((item, index) => {
-          const inputArrValues = Object.values(inputs);
-          const inputArrKeys = Object.keys(inputs);
-          let rowSum = inputArrValues[index] * item.price;
-          return (
-            <TableRow
-              key={index}
-              item={item}
-              name={inputArrKeys[index]}
-              quantity={inputArrValues[index]}
-              handleChange={handleChange}
-              sum={rowSum}
-            />
-          );
-        })}
-      </tbody>
-    </table>
+    <div className="table-wrap">
+      <table className="estimate-table">
+        <TableHeader />
+        <tbody>
+          {list.map((item, index) => {
+            const inputArrValues = Object.values(inputs);
+            const inputArrKeys = Object.keys(inputs);
+            let rowSum = inputArrValues[index] * item.price;
+            return (
+              <TableRow
+                key={index}
+                item={item}
+                name={inputArrKeys[index]}
+                quantity={inputArrValues[index]}
+                handleChange={handleChange}
+                sum={rowSum}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
