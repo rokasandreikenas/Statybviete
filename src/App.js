b887f6a794b3cbd4d513ad52b7a51a8106f45eff
rokasandreikenas <shyptt@gmail.com>
2020-11-03 14:09:01 +0200

    update router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Calculator from "./pages/Calculator";
 import Contacts from "./pages/Contacts";
@@ -17,6 +22,7 @@ const App = () => {
         <Route path="/" component={Home} exact />
         <Route path="/skaiciuokle" component={Calculator} exact />
         <Route path="/kontaktai" component={Contacts} exact />
+        <Route render={() => <Redirect to="/" />} />
       </Switch>
     </Router>
   );
